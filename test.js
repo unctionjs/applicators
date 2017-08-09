8c022265f9302ec98b9bb33bea1181f429725361
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-09 13:19:45 -0700

    Making packages actually run and test and then also flow

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ import {test} from "tap"
 import recordfrom from "@unction/recordfrom"
 import {inc} from "ramda"
 import {dec} from "ramda"
-import applicators from "./"
+import applicators from "./source.js"
 
 test(({same, end}) => {
   same(
