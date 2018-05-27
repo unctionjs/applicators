d8df1f939e8c1217b9c84ab7dbdfad4759f56f5d
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:28 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-import mapValues from "@unction/mapvalues"
-import splat from "@unction/splat"
-import applicator from "@unction/applicator"
-import zip from "@unction/zip"
-import length from "@unction/length"
-
-import type {UnaryFunctionType} from "types"
-import type {MapperFunctionType} from "types"
-
-export default function applicators (unctions: Array<MapperFunctionType> | RecordType<KeyType, MapperFunctionType>): UnaryFunctionType {
-  const zipUnctions = zip(unctions)
-
-  return function applicatorsUnctions (functor: ArrayType | RecordType): ArrayType | RecordType {
+import mapValues from "@unction/mapvalues";
+import splat from "@unction/splat";
+import applicator from "@unction/applicator";
+import zip from "@unction/zip";
+import length from "@unction/length";
+export default function applicators(unctions) {
+  const zipUnctions = zip(unctions);
+  return function applicatorsUnctions(functor) {
     if (length(unctions) !== length(functor)) {
-      throw new Error("left and right werent the same size")
+      throw new Error("left and right werent the same size");
     }
 
-    return mapValues(splat(applicator))(zipUnctions(functor))
-  }
+    return mapValues(splat(applicator))(zipUnctions(functor));
+  };
 }
