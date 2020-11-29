441b32facc051c5186bcc2c735c60206f6561df4
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-28 18:50:19 -0800

    Denormalizing some types to make things a little bit easier

--- index.ts
+++ index.ts
@@ -3,12 +3,11 @@ import splat from "@unction/splat";
 import applicator from "@unction/applicator";
 import zip from "@unction/zip";
 import length from "@unction/length";
-import {EnumerableType} from "./types";
+
 import {MapperFunctionType} from "./types";
 
 export default function applicators<A, B, C> (unctions: EnumerableType<MapperFunctionType<A, B>, C>) {
   const zipUnctions = zip(unctions);
-
 
   return function applicatorsUnctions (enumerable: EnumerableType<A, C>): EnumerableType<B, C> {
     if (length(unctions) !== length(enumerable)) {
